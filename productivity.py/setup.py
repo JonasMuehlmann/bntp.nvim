c17e2469cc4935e1d773b257ff0774a4a42558a8
jonas <jonasmuehlmann@protonmail.com>
2021-07-30 19:58:55 +0200

    ci(`travisCi`): fix release step

--- productivity.py/setup.py
+++ productivity.py/setup.py
@@ -6,7 +6,7 @@ import setuptools  # type: ignore
 with open("README.md", "r", encoding="utf-8") as readme:
     long_description = readme.read()
 
-__version__ = "0.1.0"
+__version__ = "0.1.1"
 
 setuptools.setup(
     name="pyaoi",
